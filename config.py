955ae4aadbe233918947c0a48711229ae559c8a8
MuzykaAndrii <andrymyzik@gmail.com>
2021-07-15 04:24:37 +0300

    changed environment variable name to DATABASE_URI to avoid problem with sqlalchemy

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     TOKEN = os.getenv('BOT_TOKEN')
     WEBHOOK = os.getenv('WEBHOOK')
-    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
+    DATABASE_URL = os.getenv('DATABASE_URI', 'sqlite:///' + os.path.join(basedir, 'app.db'))
